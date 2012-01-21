b50a8133ae870145d6d1646378f1a03a260dd9f0
Jacob O'Donnell <jacobodonnell@gmail.com>
2012-01-21 15:47:49 -0500

    fixed ticket link

--- spec/mailers/show_mailer_spec.rb
+++ spec/mailers/show_mailer_spec.rb
@@ -10,6 +10,6 @@ describe ShowMailer do
   it "should send mails" do
     ShowMailer.email_shows
     ActionMailer::Base.deliveries.should_not be_empty
-    ActionMailer::Base.deliveries[0].body.parts.find {|p| p.content_type.match /html/}.body.raw_source.should include("M83 on January 20, 2012 at Location.  Tickets")
+    ActionMailer::Base.deliveries[0].body.parts.find {|p| p.content_type.match /html/}.body.raw_source.should include("M83 on January 20, 2012 at Location.  <a href=\"http://google.com\">Tickets</a>")
   end
 end
